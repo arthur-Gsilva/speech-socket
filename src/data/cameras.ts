55ccbf8af0d2f2f55a845ff1adae4f6823207b95
Arthur Gabriel <122728865+arthur-Gsilva@users.noreply.github.com>
2025-05-03 00:49:52 -0300

    ajeitando url

--- src/data/cameras.ts
+++ src/data/cameras.ts
@@ -5,7 +5,7 @@ export let cameras: Camera[] = [
         id: 1,
         keyword: 'Boa viagem',
         address: 'rua da auraro 34380',
-        url: 'http://localhost:3001/stream/cam1_transcoded_output/index.m3u8',
+        url: 'https://localhost:3001/stream/cam1_transcoded_output/index.m3u8',
         inDropzone: false
 
     },
@@ -20,56 +20,56 @@ export let cameras: Camera[] = [
         id: 3,
         keyword: 'BR-101',
         address: 'Avenida norte de brasil',
-        url: 'http://localhost:3001/stream/cam6/index.m3u8',
+        url: 'https://localhost:3001/stream/cam6/index.m3u8',
         inDropzone: false
     },
     {
         id: 4,
         keyword: 'Guararapes',
         address: 'visão da rua guara',
-        url: 'http://localhost:3001/stream/cam2/index.m3u8',
+        url: 'https://localhost:3001/stream/cam2/index.m3u8',
         inDropzone: false
     },
     {
         id: 5,
         keyword: 'Rua da Aurora',
         address: 'rua da aurora 4353',
-        url: 'http://localhost:3001/stream/cam3/index.m3u8',
+        url: 'https://localhost:3001/stream/cam3/index.m3u8',
         inDropzone: false
     },
     {
         id: 6,
         keyword: 'Derby',
         address: 'Praça do Derby 7231',
-        url: 'http://localhost:3001/stream/cam4/index.m3u8',
+        url: 'https://localhost:3001/stream/cam4/index.m3u8',
         inDropzone: false
     },
     {
         id: 7,
         keyword: 'Boa vista',
         address: 'Avenida conde da boa vista',
-        url: 'http://localhost:3001/stream/cam5/index.m3u8',
+        url: 'https://localhost:3001/stream/cam5/index.m3u8',
         inDropzone: false
     },
     {
         id: 8,
         keyword: 'PE-15',
         address: 'Pernambuco numero 15',
-        url: 'http://localhost:3001/stream/cam7/index.m3u8',
+        url: 'https://localhost:3001/stream/cam7/index.m3u8',
         inDropzone: false
     },
     {
         id: 9,
         keyword: 'Torre Aurora',
         address: 'aaaa torreeee',
-        url: 'http://localhost:3001/stream/cam8_transcoded_output/index.m3u8',
+        url: 'https://localhost:3001/stream/cam8_transcoded_output/index.m3u8',
         inDropzone: false
     },
     {
         id: 10,
         keyword: 'Caruaru',
         address: 'Bairro de Kennedy',
-        url: 'http://localhost:3001/stream/cam9_transcoded_output/index.m3u8',
+        url: 'https://localhost:3001/stream/cam9_transcoded_output/index.m3u8',
         inDropzone: false
     },
     {
