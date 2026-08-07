091a9eca3a8c91c57a9beb4fed15046be833a8a9
Arthur Gabriel <122728865+arthur-Gsilva@users.noreply.github.com>
2026-08-07 13:37:27 -0300

    Update camera IDs and remove unused entry

--- src/data/cameras.ts
+++ src/data/cameras.ts
@@ -10,72 +10,58 @@ export let cameras: Camera[] = [
   },
   {
     id: 2,
-    keyword: 'Vitória',
-    address: 'rua da vitoria 34380',
-    url: 'https://34.104.32.249.nip.io/SP324-KM081/stream.m3u8',
-    inDropzone: false
-  },
-  {
-    id: 3,
     keyword: 'BR-101',
     address: 'Avenida norte de brasil',
     url: 'https://localhost:3001/stream/cam6/index.m3u8',
     inDropzone: false
   },
   {
-    id: 4,
+    id: 3,
     keyword: 'Guararapes',
     address: 'visão da rua guara',
     url: 'http://localhost:3001/stream/cam2/index.m3u8',
     inDropzone: false
   },
   {
-    id: 5,
+    id: 4,
     keyword: 'Rua da Aurora',
     address: 'rua da aurora 4353',
     url: 'http://localhost:3001/stream/cam3/index.m3u8',
     inDropzone: false
   },
   {
-    id: 6,
+    id: 5,
     keyword: 'Derby',
     address: 'Praça do Derby 7231',
     url: 'http://localhost:3001/stream/cam4/index.m3u8',
     inDropzone: false
   },
   {
-    id: 7,
+    id: 6,
     keyword: 'Boa vista',
     address: 'Avenida conde da boa vista',
     url: 'http://localhost:3001/stream/cam5/index.m3u8',
     inDropzone: false
   },
   {
-    id: 8,
+    id: 7,
     keyword: 'PE-15',
     address: 'Pernambuco numero 15',
     url: 'http://localhost:3001/stream/cam7/index.m3u8',
     inDropzone: false
   },
   {
-    id: 9,
+    id: 8,
     keyword: 'Torre Aurora',
     address: 'aaaa torreeee',
     url: 'http://localhost:3001/stream/cam8_transcoded_output/index.m3u8',
     inDropzone: false
   },
   {
-    id: 10,
+    id: 9,
     keyword: 'Caruaru',
     address: 'Bairro de Kennedy',
     url: 'http://localhost:3001/stream/cam9_transcoded_output/index.m3u8',
-    inDropzone: false
-  },
-  {
-    id: 11,
-    keyword: 'Caxangá',
-    address: 'Rua dos escravos de jó',
-    url: 'https://34.104.32.249.nip.io/SP055-KM055/stream.m3u8',
     inDropzone: false
   }
 ];
